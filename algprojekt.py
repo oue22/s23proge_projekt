429881c5b05ac903a0c1ef3a4ba845ad6f6fc5c9
Ingo <103316009+oue22@users.noreply.github.com>
2023-11-15 22:02:26 +0200

    Create algprojekt.py
    Pythonis tehtud projekti baas. Sellele on vaja juurde teha veel flaskiga veebirakendus.

---
+++ algprojekt.py
@@ -0,0 +1,34 @@
+def failist_sõnastikku(fnimi):
+    sonastik = {}
+    with open(fnimi, 'r', encoding='utf-8') as fail:
+        for e in fail:
+          andmed = e.strip().split(',')
+          if len(andmed) >= 2:
+              osaleja, arv = andmed
+              sonastik[osaleja] = int(arv) 
+    return sonastik
+    
+
+
+def hääleta_ja_salvesta(andmed):
+    print("Hääleta oma lemmiku poolt:")
+    for i, (osaleja, arv) in enumerate(andmed.items(), start=1):
+        print(str(i) + '. ' + osaleja)
+
+    valik = int(input("Vali number: "))
+
+    if 1 <= valik <= len(andmed):
+        valitud_osaleja = list(andmed.keys())[valik - 1]
+        andmed[valitud_osaleja] += 1
+        print('Sina hääletasid ' + valitud_osaleja + ' poolt.')
+
+        # Salvesta tulemused
+        with open('proje.txt', 'w', encoding='utf-8') as fail:
+            for osaleja, arv in andmed.items():
+                fail.write(osaleja + ',' + str(arv) + '\n')
+    else:
+        print("Vigane valik.")
+
+failinimi = 'proje.txt'
+andmed = failist_sõnastikku(failinimi)
+hääleta_ja_salvesta(andmed)
