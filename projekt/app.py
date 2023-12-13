5b918a7bb5702ad5b462e9983697bf4ba95f8d59
Ingo <103316009+oue22@users.noreply.github.com>
2023-12-14 00:02:43 +0200

    Uuendatud projekt
    Selles toimib kogu projekti baas funktsioonid. Nupule vajutades lisab faili selle. Praegu saab üks inimene lõputult hääletada ning ei tööta hääletuse lõpp aeg.

--- projekt/app.py
+++ projekt/app.py
@@ -3,78 +3,68 @@ from flask import Flask, render_template, request
 import random
 
 from datetime import datetime
-#Funktsioon võitja leidmiseks
-def voitja(fnimi):
-    return max(failist_sõnastikku(fnimi))
+
+
 
 #Funktsioon failist osalejate lugemiseks ja sõnastikku kirjutamiseks.
 def failist_sõnastikku(fnimi):
     sonastik = {}
     with open(fnimi, 'r', encoding='utf-8') as fail:
-        for e in fail:
-          andmed = e.strip().split(',')
-          if len(andmed) >= 2:
-              osaleja, arv = andmed
-              sonastik[osaleja] = int(arv) 
+        for rida in fail:
+            andmed = rida.strip().split(',')
+            if len(andmed) >= 2:
+                osaleja, arv = andmed
+                # Teisenda 'arv' stringist int-iks
+                sonastik[osaleja] = int(arv)
     return sonastik
- 
-def hääleta_ja_salvesta(andmed, lõpp_kuupäev):
-    #Et inimesed ei saaks peale lõppkuupäeva hääletada
-    if datetime.now() >= lõpp_kuupäev:
-        print("Hääletamine on lõppenud.")
-        return
-    
-    print("Hääleta oma lemmiku poolt:")
-    for i, (osaleja, arv) in enumerate(andmed.items(), start=1):
-        print(str(i) + '. ' + osaleja)
 
-    valik = int(input("Vali number: "))
+def voitja(fnimi):
+    sonastik = failist_sõnastikku(fnimi)
+    # Tagasta maksimaalne võitja nimi, kasutades sõnastiku väärtust
+    return max(sonastik, key=sonastik.get)
 
-    if 1 <= valik <= len(andmed):
-        valitud_osaleja = list(andmed.keys())[valik - 1]
-        andmed[valitud_osaleja] += 1
-        print('Sina hääletasid ' + valitud_osaleja + ' poolt.')
 
-        # Salvesta tulemused
-        with open('proje.txt', 'w', encoding='utf-8') as fail:
-            for osaleja, arv in andmed.items():
-                fail.write(osaleja + ',' + str(arv) + '\n')
-    else:
-        print("Vigane valik.")
+
         
-        
-        
-def sõnastikust_järjendi(andmed):
+def sõnastikust_järjendi(sonastik):
     jar = []
-    for i in andmed: 
+    for i in sonastik: 
         jar.append(i)
     return jar
 
+def hääleta_ja_salvesta(fnimi, kasutaja_hääl):
+    sonastik = failist_sõnastikku(fnimi)
+    print(kasutaja_hääl) 
+    for osaleja, tulemus in sonastik.items():
+        if osaleja == kasutaja_hääl:
+            sonastik[osaleja] += 1
+    # Salvesta tulemused
+    with open(fnimi, 'w', encoding='utf-8') as fail:
+        for osaleja, tulemus in sonastik.items():
+            fail.write(osaleja + ',' + str(tulemus) + '\n') 
 
-
-
- 
-failinimi = 'nimed.txt'
-andmed = failist_sõnastikku(failinimi)
-lõpp_kuupäev = datetime(2024, 12, 14)
-hääleta_ja_salvesta(andmed, lõpp_kuupäev)
 
 app = Flask(__name__)
  
 @app.route("/")
 def index():
-    järjend = sõnastikust_järjendi(andmed)
+    sonastik = failist_sõnastikku('nimed.txt')
+    järjend = sõnastikust_järjendi(sonastik)
     return render_template("index.html", osaleja=järjend)
 @app.route('/handle_button_click', methods=['POST'])
 def handle_button_click():
     clicked_button = request.form['clicked_button']
     print(f"Nupp {clicked_button} on vajutatud!")
-    return "Olete hääletanud2"
+    
+    #saa kasutaja vastus paremale kujule
+    kasutaja_hääl = request.form.get('clicked_button')
+    hääleta_ja_salvesta('nimed.txt', kasutaja_hääl)
+    return render_template("clicked_button.html", valik=kasutaja_hääl)
 
-@app.route("/teine")
-def teine():
-    materjal = failist_sõnastikku(failinimi)
-    return materjal
+@app.route("/tulemus")
+def tulemus():
+    esimene = voitja("nimed.txt")
+    return esimene
  
 if __name__ == "__main__":
     app.run(debug=True)
